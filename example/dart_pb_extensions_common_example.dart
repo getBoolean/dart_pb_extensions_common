7b8f9a5681a39b601cb384b6742abbad6771bb0d
getBoolean <getboolean@gmail.com>
2022-12-12 00:49:53 -0600

    Initial commit

---
+++ example/dart_pb_extensions_common_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_pb_extensions_common/dart_pb_extensions_common.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_pb_extensions_common.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_pb_extensions_common;
+
+export 'src/dart_pb_extensions_common_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
