6f84648aea9ff3427bfb4c1330f22cd0c6255095
getBoolean <getboolean@gmail.com>
2022-12-13 02:46:20 -0600

    add enum docs

--- lib/src/paperback/models/source_tag.dart
+++ lib/src/paperback/models/source_tag.dart
@@ -3,6 +3,12 @@ part of '../paperback.dart';
 @JS()
 class SourceTag {
   external String get text;
+
+  /// An enumerator which [SourceTag] uses to define the color of the tag rendered on the website.
+  /// Five types are available: blue, green, grey, yellow and red, the default one is blue.
+  /// Common colors are red for (Broken), yellow for (+18), grey for (Country-Proof)
+  ///
+  /// Use [TagType] to define the type of the tag
   external String get type;
 }
 
