a6403d991b251cb00ba7c144d40c86cb1160ed5e
getBoolean <getboolean@gmail.com>
2022-12-14 16:18:23 -0600

    Use Source future class in example

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -7,17 +7,14 @@ import 'package:dart_pb_extensions_common/js.dart';
 @JS('Guya')
 external set _createGuya(void Function() f);
 
-// @JS('GuyaInfo')
-// external set _GuyaInfo(SourceInfo Function() f);
-
 void main() {
   // Solution to allowing interop with class methods
   // https://github.com/dart-lang/sdk/issues/47855#issuecomment-1069311247
   _createGuya = allowInterop(() {
     final guya = Guya();
     final _ = jsify({
-      'getChapterDetails': allowInterop(guya.getChapterDetails),
-      'getChapters': allowInterop(guya.getChapters),
+      'getChapterDetails': allowInterop(guya.service.getChapterDetails),
+      'getChapters': allowInterop(guya.service.getChapters),
     });
   });
 
@@ -45,69 +42,62 @@ SourceInfo guyaInfo = SourceInfo(
   ],
 );
 
-class Guya extends SourcePromise {
+class Guya extends Source {
   @override
   RequestManager get requestManager => RequestManager(requestsPerSecond: 5, requestTimeout: 10000);
 
   @override
-  String? getMangaShareUrl(String mangaId) => '$kGuyaDomain/read/manga/$mangaId';
-
-  @override
-  Promise<MangaInfo> getMangaDetails(String mangaId) {
+  Future<MangaInfo> getMangaDetails(String mangaId) async {
     final request = Request(url: '$kGuyaApiBase/series_page_data/$mangaId', method: 'GET');
-    final mangaInfo = requestManager.schedule(request, 1).toFuture().then((Response response) {
-      final responseData = response.data;
-      final dynamic data = (responseData is String) ? parseJson(responseData) : response.data;
-      if (data is! Map<String, Object?>) {
-        throw JsError('Invalid response data: $responseData');
-      }
+    final response = await requestManager.schedule(request, 1).toFuture();
 
-      final series = asType<String>(data['series']);
-      final altTitles = asType<List>(data['alt_titles']) ?? [];
-      final List<String> titles = [series, ...altTitles].removeNull();
-      final coverVolUrl = data['cover_vol_url'];
-      final metadataElement = asType<Map>(data['metadata']);
-      final synopsis = asType<String>(data['synopsis']);
+    final responseData = response.data;
+    final dynamic data = (responseData is String) ? parseJson(responseData) : response.data;
+    if (data is! Map<String, Object?>) {
+      throw JsError('Invalid response data: $responseData');
+    }
 
-      return MangaInfo(
-        id: mangaId,
-        titles: titles,
-        image: coverVolUrl != null && coverVolUrl is String
-            ? kGuyaDomain + coverVolUrl
-            : 'https://i.imgur.com/GYUxEX8.png',
-        status: MangaStatus.ongoing,
-        author: metadataElement?[0]?[1],
-        artist: metadataElement?[1]?[1],
-        desc: synopsis,
-      );
-    });
+    final series = asType<String>(data['series']);
+    final altTitles = asType<List>(data['alt_titles']) ?? [];
+    final List<String> titles = [series, ...altTitles].removeNull();
+    final coverVolUrl = data['cover_vol_url'];
+    final metadataElement = asType<Map>(data['metadata']);
+    final synopsis = asType<String>(data['synopsis']);
 
-    return mangaInfo.toPromise();
+    return MangaInfo(
+      id: mangaId,
+      titles: titles,
+      image: coverVolUrl != null && coverVolUrl is String
+          ? kGuyaDomain + coverVolUrl
+          : 'https://i.imgur.com/GYUxEX8.png',
+      status: MangaStatus.ongoing,
+      author: metadataElement?[0]?[1],
+      artist: metadataElement?[1]?[1],
+      desc: synopsis,
+    );
   }
 
   @override
-  Promise<List<Chapter>> getChapters(String mangaId) {
+  Future<List<Chapter>> getChapters(String mangaId) async {
     throw UnimplementedError();
   }
 
   @override
-  Promise<ChapterDetails> getChapterDetails(String mangaId, String chapterId) {
+  Future<ChapterDetails> getChapterDetails(String mangaId, String chapterId) async {
     throw UnimplementedError();
   }
 
   @override
-  Promise<void>? filterUpdatedManga(
-      void Function(MangaUpdates updates) updates, Date time, List<String> ids) {
+  Future<void> filterUpdatedMangas(
+    void Function(MangaUpdates updates) updates,
+    Date time,
+    List<String> ids,
+  ) {
     throw UnimplementedError();
   }
 
   @override
-  Promise<void>? getHomePageSections(void Function(HomeSection section) sectionCallback) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Promise<PagedResults> getSearchResults(SearchRequest query, Object? metadata) {
+  Future<PagedResults> getSearchResults(SearchRequest query, Object? metadata) async {
     throw UnimplementedError();
   }
 }
